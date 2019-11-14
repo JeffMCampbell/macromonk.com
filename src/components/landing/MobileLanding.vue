b92a1a852a2fbf1c643cbd210851e61b9c12548f
Jeff Campbell <campbelljeffm@gmail.com>
2019-11-13 21:24:06 -0500

    wip

---
+++ src/components/landing/MobileLanding.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center">
+
+    <div class="flex items-center py-5">
+      <logo/>
+    </div>
+
+    <div class="notsure">
+
+      <div class="flex items-center justify-center p-6">
+        <button class="bg-red-light text-grey-light font-bold py-3 px-4 rounded mr-5" @click="loginHandler">Login</button>
+        <button class="bg-transparent text-red-light font-bold py-3 px-4 rounded border border-red-light" @click="createAccountHandler">Create Account</button>
+      </div>
+
+      <div class="flex flex-col items-center justify-center text-center w-full p-4">
+        <div class="text-3xl text-white text-bold mb-4">Plan Your Macros For Ingredients, Recipes, Meals and Days</div>
+        <div class="text-xl text-white text-bold mb-8">Manually enter macros from your own nutrition label!</div>
+        <div class="carousel-wrapper overflow-auto p-4">
+          <carousel :per-page="1">
+            <slide class="text-center image-slide" v-for="image in images" :key="image.path">
+              <img :src="image.path">
+            </slide>
+          </carousel>
+        </div>
+      </div>
+
+      <div class="flex flex-col items-center justify-center p-6">
+        <div class="text-2xl text-white">Features</div>
+      </div>
+
+      <ul class="list-disc text-white mb-8">
+        <li>MacroMonk is a tool to help you manage and plan your calorie and macro nutritent intake.</li>
+        <li>It helps you plan your macros for recipes, meals & days based on raw ingredients you enter into the app.</li>
+        <li>We allow you to enter the nutrition data yourself for the ingriedents you use, instead of trusting the data on various other resources.</li>
+        <li> You get to enter exactly what your label says</li>
+      </ul>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/shared/Logo'
+import { Carousel, Slide } from 'vue-carousel'
+
+export default {
+    name: 'landing-navigation',
+    components: { Logo, Carousel, Slide },
+    props: ['loginHandler', 'createAccountHandler'],
+    computed: {
+        images () {
+            return [
+                {
+                    path: '/img/landing/ingredient-list2.png'
+                },
+                {
+                    path: '/img/landing/new-ingredient2.png'
+                },
+                {
+                    path: '/img/landing/view-ingredient2.png'
+                },
+                {
+                    path: '/img/landing/view-recipe2.png'
+                },
+                {
+                    path: '/img/landing/edit-recipe2.png'
+                },
+                {
+                    path: '/img/landing/create-meal2.png'
+                },
+                {
+                    path: '/img/landing/view-meal2.png'
+                },
+                {
+                    path: '/img/landing/create-day2.png'
+                },
+                {
+                    path: '/img/landing/view-day2.png'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  .image-slide {
+      width:auto;
+      height: auto;
+      max-height: 500px;
+      overflow: auto;
+        img {
+          width: 800px;
+          height:auto;
+      }
+  }
+
+  .mobile-footer {
+    height: 50px;
+  }
+
+
+  .carousel-wrapper {
+
+  }
+
+  .notsure {
+    background: #48545d;
+  }
+
+    .auth-modal-wrapper {
+        width: 25rem;
+    }
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,60 +1,26 @@
 <template>
-  <div class="min-h-screen flex flex-col items-center">
-
-    <div class="flex w-full items-center justify-between px-20">
-      <logo/>
-      <div class="flex items-center justify-center p-6">
-        <button class="bg-red-light text-grey-light font-bold py-3 px-4 rounded mr-5" @click="toggleShowLogin">Login</button>
-        <button class="bg-transparent text-red-light font-bold py-3 px-4 rounded border border-red-light" @click="toggleShowCreateAccount">Create Account</button>
-      </div>
-    </div>
-
-    <div class="flex flex-col items-center justify-center notsure w-full p-4">
-      <div class="text-3xl text-white text-bold mt-8 mb-4">Plan Your Macros For Ingredients, Recipes, Meals and Days</div>
-      <div class="text-xl text-white text-bold mb-8">Manually enter macros from your own nutrition label!</div>
-      <div class="carousel-wrapper overflow-auto p-4">
-        <carousel :per-page="1">
-          <slide class="text-center image-slide" v-for="image in images" :key="image.path">
-            <img :src="image.path">
-          </slide>
-        </carousel>
-      </div>
-    </div>
-
-    <div class="flex flex-col items-center justify-center p-6">
-      <div class="text-2xl text-white">Features</div>
-    </div>
-
-    <ul class="list-disc text-white mb-8">
-      <li>MacroMonk is a tool to help you manage and plan your calorie and macro nutritent intake.</li>
-      <li>It helps you plan your macros for recipes, meals & days based on raw ingredients you enter into the app.</li>
-      <li>We allow you to enter the nutrition data yourself for the ingriedents you use, instead of trusting the data on various other resources.</li>
-      <li> You get to enter exactly what your label says</li>
-    </ul>
-
-
+  <div>
+    <component :is="landingComponent" :login-handler="toggleShowLogin" :create-account-handler="toggleShowCreateAccount"/>
     <modal v-if="showLogin || showCreateAccount" @close="closeAuth">
       <div class="auth-modal-wrapper">
         <login v-if="showLogin"/>
         <create-account v-if="showCreateAccount"/>
       </div>
     </modal>
-
   </div>
 </template>
 
 <script>
-import { mixin as clickaway } from 'vue-clickaway2'
+import { mapGetters } from 'vuex'
 import Modal from '@/components/shared/modals/Modal'
 import Login from '@/components/landing/Login'
 import CreateAccount from '@/components/landing/CreateAccount'
-import Logo from '@/components/shared/Logo'
-import { Carousel, Slide } from 'vue-carousel'
+import DesktopLanding from '@/components/landing/DesktopLanding'
+import MobileLanding from '@/components/landing/MobileLanding'
 
 export default {
-    name: 'landing-navigation',
-    components: { Logo, Modal, Login, CreateAccount, Carousel, Slide },
-    mixins: [ clickaway ],
+    name: 'landing',
+    components: { DesktopLanding, MobileLanding, Modal, CreateAccount, Login },
     data () {
         return {
             showLogin: false,
@@ -62,36 +28,11 @@ export default {
         }
     },
     computed: {
-        images () {
-            return [
-                {
-                    path: '/img/landing/ingredient-list2.png'
-                },
-                {
-                    path: '/img/landing/new-ingredient2.png'
-                },
-                {
-                    path: '/img/landing/view-ingredient2.png'
-                },
-                {
-                    path: '/img/landing/view-recipe2.png'
-                },
-                {
-                    path: '/img/landing/edit-recipe2.png'
-                },
-                {
-                    path: '/img/landing/create-meal2.png'
-                },
-                {
-                    path: '/img/landing/view-meal2.png'
-                },
-                {
-                    path: '/img/landing/create-day2.png'
-                },
-                {
-                    path: '/img/landing/view-day2.png'
-                }
-            ]
+        ...mapGetters({
+            isDesktop: 'isDesktop'
+        }),
+        landingComponent () {
+            return this.isDesktop ? 'DesktopLanding' : 'MobileLanding'
         }
     },
     methods: {
@@ -112,27 +53,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .image-slide {
-      width:auto;
-      height: auto;
-      max-height: 500px;
-      overflow: auto;
-        img {
-          width: 800px;
-          height:auto;
-      }
-  }
-
-
-  .carousel-wrapper {
-
-  }
-
   .notsure {
     background: #48545d;
   }
 
-    .auth-modal-wrapper {
-        width: 25rem;
-    }
+  .auth-modal-wrapper {
+      width: 25rem;
+  }
 </style>
